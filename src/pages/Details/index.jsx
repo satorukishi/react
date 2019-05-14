548b80cd07504e0a243225370fe521b893f838ad
satorukishi <34252691+satorukishi@users.noreply.github.com>
2019-05-13 22:54:05 -0300

    Tela de Search funcionando

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,16 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 
 class Details extends Component {
     constructor(props) {
         super(props);
-        this.properties = props;
-        console.log(this.props);
+        this.state = {
+            id: props.match.params.id,
+            loading: true,
+            data: {},
+        };
+    }
+
+    componentDidMount() {
+        axios.all([
+            axios.get(`https://api.mercadolibre.com/items/${this.state.id}`),
+            axios.get(`https://api.mercadolibre.com/items/${this.state.id}/description`)
+        ])
+            .then(([item, description]) => { // { data } é um destructor
+                console.log(item, description);
+
+                // auto-assign
+                this.setState({ // sempre que chamar o setState, ele chama o render() novamente
+                    data: {
+                        ...item.data, // ... = spread (joga tudo que tem dentro de item.data na data: )
+                        description: description.data.plain_text,
+                    },
+                    loading: false,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    renderConteudo() {
+        const { data } = this.state;
+        return (
+            <Fragment>
+                <section className="mdl-layout section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+                    <header className="mdl-cell mdl-cell--6-col-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone">
+                        <img src={ data.pictures[0].url } />
+                    </header>
+                    <div className="mdl-card mdl-cell mdl-cell--6-col-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone">
+                        <div className="mdl-card__supporting-text">
+                            <h5>{ data.title }</h5>
+                            { data.price}
+                        </div>
+                        <button>Comprar</button>
+                    </div>
+                    <p className="mdl-cell mdl-cell--12-col-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone ">
+                        { data.description}
+                    </p>
+                </section>
+            </Fragment>
+        );
     }
 
     render() {
-        return (
-            <p>Details: { this.properties.match.params.id}</p>
-        );
+        const { loading } = this.state;
+        
+        return loading ?
+            <div>Carregando...</div> :
+            this.renderConteudo();
     }
 }
 
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,9 +1,62 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 class Search extends Component {
+    constructor() {
+        super();
+
+        this.onSearch = this.onSearch.bind(this);
+
+        this.state = {
+            results: [],
+        }
+    }
+
+    onSearch(event) {
+        const value = event.currentTarget.value;
+
+        if (value.length > 5) {
+            console.log(value);
+
+            axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${value}`)
+                .then(({ data }) => {
+                    console.log(data);
+
+                    this.setState({
+                        results: data.results,
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    }
+
     render() {
         return (
-            <div>Search</div>
+            <div>
+                <input type="text" onChange={ this.onSearch } />
+
+                <ul>
+                    {this.state.results.map(this.renderItem) }
+                </ul>
+
+            </div>
+
+        );
+    }
+
+    renderItem(item) {
+        return (
+            <li key={ item.id }>
+                <span>{ item.id }</span>
+                <span>{ item.title }</span>
+                &#160;
+                <Link to={ `/Details/${item.id}` }>
+                    Abrir produto
+                </Link>
+            </li>
         );
     }
 }
